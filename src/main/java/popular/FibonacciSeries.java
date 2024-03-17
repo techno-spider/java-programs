5ad8e95cd99b338dcf347e903db34a3b80265a89
The owner <patriotindian789@gmail.com>
2024-03-18 02:09:50 +0530

    changes done

--- src/main/java/popular/FibonacciSeries.java
+++ src/main/java/popular/FibonacciSeries.java
@@ -24,7 +24,7 @@ public class FibonacciSeries {
         System.out.println("The Fibonacci series up to the " + n + " number is: ");
         System.out.print(first + ", " + second);
 
-        for (int i = 2; i < n; i++) {
+        for (int i = 2; i <= n; i++) {
             int next = first + second;
             System.out.print(", " + next);
             first = second;
@@ -40,6 +40,6 @@ public class FibonacciSeries {
 /*
  * output:
  * The Fibonacci series up to the 10 number is:
- * 0, 1, 1, 2, 3, 5, 8, 13, 21, 34
- * The 10 fibonacci number is: 34
+ * 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55
+ * The 10 fibonacci number is: 55
  */
